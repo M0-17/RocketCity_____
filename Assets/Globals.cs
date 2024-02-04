c05b9be42a88fe96ddaf2032cc108892d257fbed
Slamjam30 <89863160+Slamjam30@users.noreply.github.com>
2024-02-04 11:46:03 -0600

    push again

--- Assets/Globals.cs
+++ Assets/Globals.cs
@@ -15,7 +15,7 @@ public class Globals : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        canJetpack = true;
+        
     }
 
     // Update is called once per frame
