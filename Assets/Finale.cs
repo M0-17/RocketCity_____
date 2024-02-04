9ab4c906d9a8eedfa72fa1a6d385731ffe413a49
Slamjam30 <89863160+Slamjam30@users.noreply.github.com>
2024-02-04 11:59:07 -0600

    last push

--- Assets/Finale.cs
+++ Assets/Finale.cs
@@ -1,48 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Finale : MonoBehaviour
 {
-    private GameObject player;
-    public Vector2 position;
-    private bool animStarted;
-    public float endTime;
-    private float trackTime;
-    public Animator am;
-    public Animator am2;
-    private GameObject cam;
+    
     // Start is called before the first frame update
     void Start()
     {
-        cam = GameObject.FindWithTag("MainCamera");
-        player = GameObject.FindWithTag("Player");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (animStarted)
-        {
-            trackTime += Time.deltaTime;
-            if (trackTime >= endTime)
-            {
-                cam.transform.position = new Vector3(cam.transform.position.x, cam.transform.position.y + (trackTime/1.2f), cam.transform.position.z);
-            }
-        }
+        
 
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        cam.GetComponent<CamFollow>().shouldFollow = false;
-        cam.transform.position = position;
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("MainMenu");
 
-        //turn on animation
-        //am.SetTrigger("");
-        animStarted = true;
-
-
+        }
 
     }
 
